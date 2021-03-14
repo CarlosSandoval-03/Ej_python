fa81d0bc2814203da4f58adad50cf688297fb685
Carlos Sandoval <csandovalc@unal.edu.co>
2021-03-13 21:24:16 -0500

    Se realiza hasta el punto 9

--- numericos.py
+++ numericos.py
@@ -5,7 +5,7 @@ def ejCinco(a, b):
 #6. Una función que determine si un número es divisible por otro.
 def ejSeis(a, b):
     resultado = a % b
-    if resultado > 0:
+    if resultado != 0:
         print("No es divisible")
     else:
         print("Es divisible")
@@ -21,8 +21,29 @@ def ejSiete(num, d=2):
         return False
 #8. Dados dos naturales, determinar si son primos relativos.
 def ejOcho(a, b):
-
+    def mcd(x, y):
+        if x > y:
+            if x % y == 0:
+                return y
+            else:
+                return mcd(y, x%y)
+        else:
+            return mcd(y,x)
+    if mcd(a,b) == 1:
+        print("Son primos relativos")
+    else:
+        print("No son primos relativos")
 #9. Determinar si un número es múltiplo de la suma de otros dos números.
+def ejNueve(a, b, c):
+    num = a + b
+    if num > c:
+        print(c,"no es multiplo de",num)
+    else:
+        if c % num == 0:
+            multi = c / num
+            print(c, "es multiplo de", num,"(El numero",int(multi),"es el multiplo)")
+        else:
+            print(c,"no es multiplo de",num)        
 #10. Dados los coeficientes de un polinomio de grado dos, evaluar el polinomio en un valor dado.
 #11. Dados los coeficientes de un polinomio de grado dos, calcular coeficiente lineal de la derivada.
 #12. Dados los coeficientes de un polinomio de grado dos y un número real, evaluar la derivada del polinomio en ese número.
@@ -32,4 +53,5 @@ def ejOcho(a, b):
 #ejCinco(int(input("Ingrese el valor de A:")), int(input("Ingrese el valor de B:")))
 #ejSeis(int(input("Ingrese el valor de A:")), int(input("Ingrese el valor de B:")))
 #ejSiete(int(input("Elija el numero: ")))
-ejOcho(3, 8)
+#ejOcho(a = int(input("Ingrese el valor de A:")), b = int(input("Ingrese el valor de B:")))
+#ejNueve(a = int(input("Ingrese el numero a sumar A: ")), b = int(input("Ingrese el numero a sumar B: ")), c = int(input("Ingrese la incognita (posible multiplo): ")))
