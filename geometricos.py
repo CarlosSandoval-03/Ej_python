bbd6063654bbe00de85276bed2cc27857f97291b
Carlos Sandoval <csandovalc@unal.edu.co>
2021-03-15 12:46:45 -0500

    Se finaliza geometricos

--- geometricos.py
+++ geometricos.py
@@ -68,10 +68,15 @@ def ejDiezSiete(r):
         print("\nProblema seleccionado fuera de rango.")
 #18. Si una araña utiliza un patrón de hexágono regular para su telaraña, y cada hexágono está separado del otro por 1cm, y la araña quiere hacer una telaraña de πr(2)
 #¿qué cantidad de telaraña requiere la araña?.
-
-
+def ejDiezOcho(r):
+    pi = 3.14159265359
+    t = pi * (r**2)
+    perimetro = 6 * r
+    cm = t / perimetro
+    print("La araña necesitara:","{:.2f}".format(cm),"cm")
 ### Pruebas funciones ###
 #ejCatorce(-1,1)
 #ejQuince(3,3,2,2)
 #ejDiezSeis(2)
-#ejDiezSiete(r = = float(input("Ingrese el radio: ")))
+#ejDiezSiete(r = float(input("Ingrese el radio: ")))
+#ejDiezOcho(r = float(input("Ingrese el radio: ")))
