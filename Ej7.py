e0b83c4dd464fd5eb7042d283a1fb96a199b96a9
Carlos Sandoval <78392526+CarlosSandoval-03@users.noreply.github.com>
2021-04-13 15:07:18 -0500

    Add negative numbers

--- Ej7.py
+++ Ej7.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #7. Determinar si un numero es primo.
 def ejSiete(numero, i=2):
-    if i >= numero:
+    if i >= numero and numero > 0:
         print("Es primo")
         return True
     elif numero%i != 0:
