4ab51c198b4850d6e678bac602796dd93ba30351
Carlos Sandoval <78392526+CarlosSandoval-03@users.noreply.github.com>
2021-04-16 17:14:46 -0500

    Update polynomial degree two

--- Ej10.py
+++ Ej10.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #10. Dados los coeficientes de un polinomio de grado dos, evaluar el polinomio en un valor dado. Ax^2+bx+c
-def ejDiez(a,b,c,x):
-    resultado = (a * (x*x)) + (b * x) + c
+def ejDiez(a,b,c):
+    x = 2
+    resultado = (a * (x**2)) + (b * x) + c
     print("El valor dado es:",resultado)
     
 def main():
     coefA = int(input("Ingrese coeficiente A: "))
     coefB = int(input("Ingrese coeficiente B: "))
     coefC = int(input("Ingrese coeficiente C: "))
-    valor = int(input("Ingrese el valor dado a X:"))
-    ejDiez(coefA,coefB,coefC,valor)
+    ejDiez(coefA,coefB,coefC)
 
 if __name__ == "__main__":
     main()
