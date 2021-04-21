fcd1484a7f067ce2b876bfbaa8194505b3ff2d06
Carlos Sandoval <csandovalc@unal.edu.co>
2021-04-21 10:37:10 -0500

    Se crea funcion de impresion y se imprimen los mensajes

--- Ej1.py
+++ Ej1.py
@@ -3,23 +3,31 @@
 #En una granja se crı́an un número de V - Vacas, A - Aves (pollos y gallinas) y E - escorpiones.
 #Las vacas están encerradas en un corral de N × M metros cuadrados, las aves en un galpón y los escorpiones en vitrinas.
 #1. Si una vaca necesita M metros cuadrados de pasto para producir X litros de leche, ¿cuántos litros de leche se producen en la granja?.
-def ejUno(vacas,corralN,corralM):
+def ejUno(vacas,corralN,corralM, producir):
     if corralM <= 0 or corralN <= 0:
-        return "Error: El area debe ser positiva"
+        return impresionMensajes(0)
     else:
         corralArea = corralN * corralM
         if vacas > 0:
-            producir = float(input("Cuantos litros de leche la vaca produce por metro cuadrado?: "))
             leche = (corralArea / producir) * float(vacas)
-            return "En la granja se producen:", leche, "litros de leche"
+            return impresionMensajes(1,leche)
         else:
-            return "No es posible producir leche sin vacas"
+            return impresionMensajes(2)
+
+def impresionMensajes(x,value = 0.0):
+    if x == 0:
+        print("Error: El area debe ser positiva")
+    elif x == 1:
+        print("En la granja se producen:", value, "litros de leche")
+    else:
+        print("No es posible producir leche sin vacas")
 
 def main():
     a = int(input("Numero de vacas: "))
     b = float(input("Ingrese el alto del corral: "))
     c = float(input("Ingrese el ancho del corral: "))
-    print(ejUno(a,b,c))
+    d = float(input("Cuantos litros de leche la vaca produce por metro cuadrado?: "))
+    ejUno(a,b,c,d)
 
 if __name__ == "__main__":
     main()
