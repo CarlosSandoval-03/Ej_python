bf05ce2d32f7b59fef91423d5e9a1beac896c5ef
Carlos Sandoval <csandovalc@unal.edu.co>
2021-05-03 13:37:59 -0500

    Se elimina retorno de caracteres por variable de evaluacion

--- Ej7.py
+++ Ej7.py
@@ -1,17 +1,25 @@
 #7. Determinar si un numero es primo.
 def ejSiete(numero, i=2):
     if i >= numero and numero > 0:
-        return "Es primo"
+        return True
     elif numero%i != 0:
         return ejSiete(numero, i + 1)
     elif numero < 0:
-        return "El valor indicado no puede ser evaluado"
+        return False
     else:
-        return "No es primo", i, "es divisor"
-    
+        return i
+
+def mensajes(*args):
+    if args[0] == True:
+        print("Es primo")
+    elif args[0] == False and len(args) == 1:
+        print("El valor indicado no puede ser evaluado")
+    elif (args != True or args != False):
+        print("No es primo",args[0],"es divisor")
+
 def main():
     a = int(input("Elija el numero: "))
-    print(ejSiete(a))
+    mensajes(ejSiete(a))
     
 if __name__ == "__main__":
     main()
