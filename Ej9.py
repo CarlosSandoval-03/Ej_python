90ba810071b92b2aa481e579677ed496bfda11f8
Carlos Sandoval <csandovalc@unal.edu.co>
2021-05-03 14:12:16 -0500

    Se elimina retorno de caracteres en funcion de evaluacion

--- Ej9.py
+++ Ej9.py
@@ -2,21 +2,35 @@
 def ejNueve(a, b, c):
     numero = a + b
     if numero > c and numero != 0:
-        return c,"no es multiplo de",numero
+        return (1,c,numero)
     elif numero == 0:
-        return "0 es multiplo de todos los numeros"
+        return (2)
     else:
         if c % numero == 0:
             multiplo = c / numero
-            return c, "es multiplo de", numero,"(El numero",int(multiplo),"es el multiplo)"
+            return (3,c,numero,multiplo)
         else:
-            return c,"no es multiplo de",numero
+            return (4,c,numero)
+
+def mensajes(args) -> tuple:
+    args = list(args)
+    if args[0] == 1:
+        print(args[1],"no es multiplo de",args[2])
+    elif args[0] == 2:
+        print("0 es multiplo de todos los numeros")
+    elif args[0] == 3:
+        print(args[1],"es multiplo de", args[2],"(El numero",int(args[3]),"es el multiplo)")
+    elif args[0] == 4:
+        print(args[1],"no es multiplo de",args[2])
+    else:
+        print("Error")
 
 def main():
     a = int(input("Ingrese el numero a sumar A: "))
     b = int(input("Ingrese el numero a sumar B: "))
     c = int(input("Ingrese la incognita (posible multiplo): "))
-    print(ejNueve(a,b,c))
+    mensajes(ejNueve(a,b,c))
 
 if __name__ == "__main__":
     main()
+##Eliminar mensajes
