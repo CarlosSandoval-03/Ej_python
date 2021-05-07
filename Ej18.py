3457d40984607578ecac6092b01d79303fa251cf
Carlos Sandoval <78392526+CarlosSandoval-03@users.noreply.github.com>
2021-05-06 21:06:36 -0500

    Cambio nombre variable

--- Ej18.py
+++ Ej18.py
@@ -2,7 +2,7 @@ import math
 #18. Si una araña utiliza un patrón de hexágono regular para su telaraña, y cada hexágono está separado del otro por 1cm, y la araña quiere hacer una telaraña de πr(2)
 #¿qué cantidad de telaraña requiere la araña?.
 def ejDiezOcho(r):
-    t = (math.pi * (r**2)) + 1
+    x = (math.pi * (r**2)) + 1
     perimetro = 6 * r
     cm = (t / perimetro)
     return "La araña necesitara:","{:.2f}".format(cm),"cm"
