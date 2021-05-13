f25cada55abaa0d057ff44a340d53d3bd6a83deb
Carlos Sandoval <csandovalc@unal.edu.co>
2021-05-12 21:05:57 -0500

    Se finaliza mcd

--- Ej33.py
+++ Ej33.py
@@ -1,17 +1,23 @@
 from Ej30 import nuevoMeterNumeros
-from Ej22 import criba  # Lista Criba
-from Ej27 import maximoArreglo  # Mayor Numero
+from Ej8 import mcdDosValores
 # Hacer un algoritmo que calcule el Maximo Comun Divisor (MCD) para un arreglo de enteros positivos.
 # Ejemplo.
 #Arreglo: (12,20,14,124,72,2458)
 # MCD del arreglo: 2
-def mcd(arreglo) -> list:
-    pass
+
+
+def mcdArreglo(arreglo):
+    num1 = arreglo[0]
+    num2 = arreglo[1]
+    mcd = mcdDosValores(num1, num2)
+    for i in range(2, len(arreglo)):
+        mcd = mcdDosValores(mcd, arreglo[i])
+    return mcd
 
 
 def main():
     lista = nuevoMeterNumeros()
-    print(lista)
+    print("El maximo comun divisor de", lista, "es", mcdArreglo(lista))
 
 
 if __name__ == "__main__":
