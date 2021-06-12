4ad5280c54364d1471eef5f5db7b189fbfc84590
Carlos Sandoval <csandovalc@unal.edu.co>
2021-06-11 19:13:27 -0500

    Nueva funcion creacion de conjunto Se finaliza ejercicio simetria

--- Ej60a70.py
+++ Ej60a70.py
@@ -1,6 +1,9 @@
 from metodosTemp import limpiarConsola
 from metodosTemp import leer_matriz_enteros
 from Ej30 import nuevoMeterNumeros
+from Ej35a42 import verificionNoRepeticion
+from metodosTemp import inversoArreglo
+from metodosTemp import inSimple
 
 
 def verificar_matriz(producto_cartesiano:list, relacion:list) -> bool:
@@ -20,11 +23,97 @@ def crear_matriz_binaria(arreglo1:list, arreglo2:list, relacion:list) -> list:
                 matriz[i][j] = 1
     return matriz
 
+# Union
 
 def union_binaria(matriz1:list, matriz2:list) -> list:
+    temp = [[0 for x in range(len(matriz1[0]))] for y in range(len(matriz1))]
+
+    for fila in range(len(temp)):
+        for columna in range(len(temp[0])):
+            if matriz1[fila][columna] == 1 or matriz2[fila][columna] == 1:
+                temp[fila][columna] = 1
+    return temp
+
+# Interseccion
+
+def interseccion_binaria(matriz1:list, matriz2:list) -> list:
+    temp = [[0 for x in range(len(matriz1[0]))] for y in range(len(matriz1))]
+
+    for fila in range(len(temp)):
+        for columna in range(len(temp[0])):
+            if matriz1[fila][columna] == 1 and matriz2[fila][columna] == 1:
+                temp[fila][columna] = 1
+    return temp 
+
+
+# Conversion a conjunto
+
+def conversion_conjunto(matriz:list) -> list:
+    lista = []
+    for i in range(len(matriz)):
+        for k in range(len(matriz[i])):
+            lista.append(matriz[i][k])
+    return verificionNoRepeticion(lista)
+
+# Simetria
+
+def simetria_binaria(matriz:list, relacion:list) -> bool:
+    lista = conversion_conjunto(matriz)
+    
+    for i in range(len(relacion)):
+        temp = inversoArreglo(relacion[i])
+        if not inSimple(temp,relacion):
+            return False
+    return True
+
+# Reflexibilidad
+
+def reflexibilidad_binaria(matriz:list, relacion:list) -> bool:
+    diccionario = {}
+    lista = conversion_conjunto(matriz) 
+
+    for j in range(len(lista)):
+        diccionario[(lista[j],lista[j])] = 0
+    
+    for fila in range(len(relacion)):
+        for key in diccionario.keys():
+            if tuple(relacion[fila]) == key:
+                diccionario[key] = diccionario.get(key, 0) + 1
+
+    for value in diccionario.values():
+        if value == 0:
+            return False
+    return True
+
+# Transitividad
+
+def transitividad_binaria(relacion:list) -> bool:
     pass
 
+# Orden
 
+def orden_binaria(relacion:list) -> bool:
+    pass
+
+# Equivalencia
+
+def equivalencia_binaria(relacion:list) -> bool:
+    pass
+
+# Funcion
+
+def funcion_binaria(relacion:list) -> bool:
+    pass
+
+# Inyectividad
+
+def inyectividad_binaria(relacion:list) -> bool:
+    pass
+
+# Sobreyectividad
+
+def sobreyectividad_binaria(relacion:list) -> bool:
+    pass
 
 
 def eleccion() -> int:
@@ -32,15 +121,31 @@ def eleccion() -> int:
     return e
 
 
-def menu(matriz:list) -> str:
+def menu(matriz1:list, matriz2:list, relacionA:list, relacionB:list) -> str:
     flag = True
     while flag:
-        pass
+        x = eleccion()
+        if x == 1:
+            temp = union_binaria(matriz1, matriz2)
+            operacion = 'union binaria'
+        elif x == 2:
+            temp = interseccion_binaria(matriz1, matriz2)
+            operacion = 'interseccion binaria'
+        elif x >= 11: 
+            flag = False
+        return f'El resultado de la operacion {operacion} es: {temp}'
 
 
 def main():
     limpiarConsola()
+    print('union:',union_binaria([[1,0,1],[0,1,0]],[[0,1,0],[1,1,0]]))
+    print('interseccion:',interseccion_binaria([[1,0,1],[0,1,0]],[[0,1,0],[1,1,0]]))
+    print('reflexion:',reflexibilidad_binaria([[1,2,3]], [[1,1],[2,2],[3,3]]))
+    print('reflexion caso false:',reflexibilidad_binaria([[1,2,3]], [[1,2],[1,1],[2,3],[3,3]]))
+    print('simetria:',simetria_binaria([[1,2,3]], [[1,1],[2,3],[3,2],[2,2]]))
+    print('simetria caso false:', simetria_binaria([[1,2,3]], [[1,2],[2,1],[1,3]]))
 
+    '''
     print('Matriz 1')
     print('Ingrese la relacion para crear las matrices booleanas\n')
     relacion = leer_matriz_enteros(int(input('Ingrese la cantidad de relaciones que desea: ')), 2)
@@ -58,6 +163,7 @@ def main():
     array4 = nuevoMeterNumeros()
     matriz2 = crear_matriz_binaria(array1, array2, relacion)
 
+    print(menu(matriz1, matriz2)) '''
 
 
 if __name__ == '__main__':
